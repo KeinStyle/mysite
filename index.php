f48b0311655befc6a72a417aef1c6c05e76dc10f
keinstyle <igorkolesny@yandex.ru>
2020-03-16 20:17:44 +0300

    'v1.21'

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?
-echo 'hello'
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>еуые</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
+
+<section class="one">
+    <div class="block">one</div>
+    <div class="block">two</div>
+    <div class="block">three</div>
+</section>
+
+</body>
+</html>
